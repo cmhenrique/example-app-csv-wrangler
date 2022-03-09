4746ae0341a78d1484b08214e77aa6788776d8cf
cmhenrique <84073632+cmhenrique@users.noreply.github.com>
2022-03-08 21:44:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,137 +1,280 @@
+#load the libraries
 import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OrdinalEncoder
 
-###################################
-from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from st_aggrid.shared import JsCode
+from sksurv.datasets import load_gbsg2
+from sksurv.preprocessing import OneHotEncoder
+from sksurv.ensemble._forest import RandomSurvivalForest
 
-###################################
+from collections import Counter
+import pandas_profiling as pp
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble._forest import RandomForestClassifier
+from sklearn import model_selection
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import statsmodels.api as sm
 
-from functionforDownloadButtons import download_button
+from joblib import dump, load
 
-###################################
+## HEADER
+from PIL import Image
+titleimg = Image.open('models/title.png')
 
+st.set_page_config(layout="wide")
 
-def _max_width_():
-    max_width_str = f"max-width: 1800px;"
-    st.markdown(
-        f"""
+st.markdown("""
+<style>
+.big-font {
+    font-size:25px !important; }
+.texto {
+    font-size:13px;
+    color:#FFF }
+#texto {
+    background-color: #ff4b4b;
+    padding:5px;
+    border-radius:5px;
+}
+.subtitulo {
+    font-size:17px;
+}
+#subtitulo {
+    margin-bottom:0px;
+    z-index: 100;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.markdown(f'''
     <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True,
-    )
+        section[data-testid="stSidebar"] .css-ng1t4o {{width: 17rem;}}
+        section[data-testid="stSidebar"] .css-1d391kg {{width: 17rem;}}
+    </style>
+''',unsafe_allow_html=True)
 
-st.set_page_config(page_icon="✂️", page_title="CSV Wrangler")
-
-# st.image("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/balloon_1f388.png", width=100)
-st.image(
-    "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/scissors_2702-fe0f.png",
-    width=100,
-)
-
-st.title("CSV Wrangler")
-
-# st.caption(
-#     "PRD : TBC | Streamlit Ag-Grid from Pablo Fonseca: https://pypi.org/project/streamlit-aggrid/"
-# )
+st.markdown('<b class="big-font">Predicting post-operative evolution of patients with Chronic Rhinosinusitis</b>', unsafe_allow_html=True)
 
 
-# ModelType = st.radio(
-#     "Choose your model",
-#     ["Flair", "DistilBERT (Default)"],
-#     help="At present, you can choose between 2 models (Flair or DistilBERT) to embed your text. More to come!",
-# )
+col1,col2,col3 = st.columns([1,1,2]) 
 
-# with st.expander("ToDo's", expanded=False):
-#     st.markdown(
-#         """
-# -   Add pandas.json_normalize() - https://streamlit.slack.com/archives/D02CQ5Z5GHG/p1633102204005500
-# -   **Remove 200 MB limit and test with larger CSVs**. Currently, the content is embedded in base64 format, so we may end up with a large HTML file for the browser to render
-# -   **Add an encoding selector** (to cater for a wider array of encoding types)
-# -   **Expand accepted file types** (currently only .csv can be imported. Could expand to .xlsx, .txt & more)
-# -   Add the ability to convert to pivot → filter → export wrangled output (Pablo is due to change AgGrid to allow export of pivoted/grouped data)
-# 	    """
-#     )
-# 
-#     st.text("")
+@st.cache(allow_output_mutation=True)
+def model():
+    df = pd.read_csv("data7.csv")
+    df2 = df.drop(["sexo", "recidiva", "data_retorno", "data_cirurgia", "intervalo", "LM2"], axis=1)
+    df3 = OneHotEncoder().fit_transform(df2)
+    Xt= df3
+    feature_names = Xt.columns.tolist()
 
+    y = []
+    for x in df.intervalo:
+        if x > 8:
+            a = False
+        else:
+            a = True
+            
+        y.append((a, x))
 
-c29, c30, c31 = st.columns([1, 6, 1])
+    dt = np.dtype([('cens', np.bool), ('time', np.float64, 1)])
+    y = np.array(y, dtype=dt)
 
-with c30:
+    X_train, X_test, y_train, y_test = train_test_split(Xt, y, test_size=0.3, random_state=100)
 
-    uploaded_file = st.file_uploader(
-        "",
-        key="1",
-        help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
-    )
+    rsf = RandomSurvivalForest(n_estimators=30, random_state=12,max_depth=1)
+    rsf.fit(X_train, y_train)
+    rsf.score(X_test, y_test)
 
-    if uploaded_file is not None:
-        file_container = st.expander("Check your uploaded .csv")
-        shows = pd.read_csv(uploaded_file)
-        uploaded_file.seek(0)
-        file_container.write(shows)
+    return rsf, feature_names
 
-    else:
-        st.info(
-            f"""
-                👆 Upload a .csv file first. Sample to try: [biostats.csv](https://people.sc.fsu.edu/~jburkardt/data/csv/biostats.csv)
-                """
-        )
+def predict():
+    data = pd.read_csv("data7.csv") 
 
-        st.stop()
+    X=data[['BIC', 'BIOFILME', 'ASMA','AERD', 'POLIPO', 'CULTURA', 'STAPHYLO', 'LM2', 'EOSINOFILICO']]  # Features
+    y=data['recidiva']  # Labels
 
-from st_aggrid import GridUpdateMode, DataReturnMode
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=100)
+    
+    ## LOGISTIC REGRESSION
+    lr = LogisticRegression()
+    model = lr.fit(X_train, y_train)
+    lr_predict = lr.predict(X_test)
+    lr_conf_matrix = confusion_matrix(y_test, lr_predict)
+    lr_acc_score = accuracy_score(y_test, lr_predict)
+    
+    ## RANDOM FOREST
+    rf = RandomForestClassifier(n_estimators=30, random_state=12,max_depth=1)
+    rf.fit(X_train,y_train)
+    rf_predicted = rf.predict(X_test)
+    rf_conf_matrix = confusion_matrix(y_test, rf_predicted)
+    rf_acc_score = accuracy_score(y_test, rf_predicted)
 
-gb = GridOptionsBuilder.from_dataframe(shows)
-# enables pivoting on all columns, however i'd need to change ag grid to allow export of pivoted/grouped data, however it select/filters groups
-gb.configure_default_column(enablePivot=True, enableValue=True, enableRowGroup=True)
-gb.configure_selection(selection_mode="multiple", use_checkbox=True)
-gb.configure_side_bar()  # side_bar is clearly a typo :) should by sidebar
-gridOptions = gb.build()
+    return lr
 
-st.success(
-    f"""
-        💡 Tip! Hold the shift key when selecting rows to select multiple rows at once!
-        """
-)
+with col1:
+    st.markdown("Pacient 1 Clinical variables associated")
+    lm =              st.slider("Lund-Mackay Score", 0, 20, 10, 1, key=1)
+    bf =         st.checkbox('Bacterial Biofilm', key=1)
+    bic =         st.checkbox('Intracellular Bacteria', key=1)
+    aerd =            st.checkbox('AERD', key=1)
+    asma =            st.checkbox('Asthma', key=1)
+    cultura =         st.checkbox('Bacterial Culture', key=1)
+    polipo =         st.checkbox('Nasal Polyps', key=1)
+    eosinofilico =        st.checkbox('Eosinophilic Polyps', key=1)
+    staphylo =        st.checkbox('Staphylococcus aureus', key=1)
 
-response = AgGrid(
-    shows,
-    gridOptions=gridOptions,
-    enable_enterprise_modules=True,
-    update_mode=GridUpdateMode.MODEL_CHANGED,
-    data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
-    fit_columns_on_grid_load=False,
-)
+with col2:
+    st.markdown("Pacient 2 Clinical variables associated")
+    lm2 =              st.slider("Lund-Mackay Score", 0, 20, 10, 1)
+    bf2 =         st.checkbox('Bacterial Biofilm')
+    bic2 =         st.checkbox('Intracellular Bacteria')
+    aerd2 =            st.checkbox('AERD')
+    asma2 =            st.checkbox('Asthma')
+    cultura2 =         st.checkbox('Bacterial Culture')
+    polipo2 =         st.checkbox('Nasal Polyps')
+    eosinofilico2 =        st.checkbox('Eosinophilic Polyps')
+    staphylo2 =        st.checkbox('Staphylococcus aureus')
+    predizer = st.button('Predict patient outcome')
 
-df = pd.DataFrame(response["selected_rows"])
+with col3:
+    st.info("Kaplan Meier Curves")
 
-st.subheader("Filtered data will appear below 👇 ")
-st.text("")
+if bf:
+    bf = 1
+else:
+    bf =  0
 
-st.table(df)
+if bic:
+    bic = 1
+else:
+    bic =  0
 
-st.text("")
+if aerd:
+    aerd = 1
+else:
+    aerd =  0
 
-c29, c30, c31 = st.columns([1, 1, 2])
+if asma:
+    asma = 1
+else:
+    asma =  0
 
-with c29:
+if cultura:
+    cultura = 1
+else:
+    cultura =  0
 
-    CSVButton = download_button(
-        df,
-        "File.csv",
-        "Download to CSV",
-    )
+if polipo:
+    polipo = 1
+else:
+    polipo = 0
 
-with c30:
-    CSVButton = download_button(
-        df,
-        "File.csv",
-        "Download to TXT",
-    )
+if eosinofilico:
+    eosinofilico = 1
+else:
+    eosinofilico =  0
+
+if staphylo:
+    staphylo = 1
+else:
+    staphylo =  0
+
+if lm >= 15:
+    lm12 = 1
+else:
+    lm12 = 0
+
+if bf2:
+    bf2 = 1
+else:
+    bf2 =  0
+
+if bic2:
+    bic2 = 1
+else:
+    bic2 =  0
+
+if aerd2:
+    aerd2 = 1
+else:
+    aerd2 =  0
+
+if asma2:
+    asma2 = 1
+else:
+    asma2 =  0
+
+if cultura2:
+    cultura2 = 1
+else:
+    cultura2 =  0
+
+if polipo2:
+    polipo2 = 1
+else:
+    polipo2 = 0
+
+if eosinofilico2:
+    eosinofilico2 = 1
+else:
+    eosinofilico2 =  0
+
+if staphylo2:
+    staphylo2 = 1
+else:
+    staphylo2 =  0
+
+if lm2 >= 15:
+    lm22 = 1
+else:
+    lm22 = 0
+
+if (predizer):
+    texto = "Hello World"
+    rsf, feature_names = model()
+    rf = predict()
+
+    paciente = [[bic, bf, asma, aerd, lm, polipo, eosinofilico, cultura, staphylo]]
+    pct = pd.DataFrame(paciente, columns = feature_names)
+
+    paciente2 = [[bic2, bf2, asma2, aerd2, lm2, polipo2, eosinofilico2, cultura2, staphylo2]]
+    pct2 = pd.DataFrame(paciente2, columns = feature_names)
+
+    with col3:
+        st.subheader(" ")
+    
+        surv = rsf.predict_survival_function(pct, return_array=True)
+        for i, s in enumerate(surv):
+            plt.step(rsf.event_times_, s, where="post", label="Pacient 1")
+            prob = s
+
+        surv2 = rsf.predict_survival_function(pct2, return_array=True)
+
+        for i, s in enumerate(surv2):
+            plt.step(rsf.event_times_, s, where="post", label="Pacient 2")
+
+        plt.ylabel("Survival probability")
+        plt.xlabel("Time in years")
+        plt.legend()
+        plt.tight_layout()
+        plt.grid(True)
+        from io import BytesIO
+
+        #st.text(prob[-1])
+        #fig, ax = plt.subplots(figsize=(5, 5))
+        #st.pyplot(fig = plt, use_container_width=True)
+        fig = plt
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+        st.image(buf, width = 450)
+
+        st.metric(label="1 Year", value="0.8")
+        st.metric(label="5 Years", value="0.5")
+        st.metric(label="8 Years", value="0.2")
